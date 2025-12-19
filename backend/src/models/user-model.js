80f09d74ba08795f081b51290243eccc2e2bf659
pdlsagar677 <sagarpoudel677@gmail.com>
2025-12-19 12:43:45 +0545

    admin updated in models

--- backend/src/models/user-model.js
+++ backend/src/models/user-model.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   profilePic: { type: String, default: null },
+  isAdmin: { type: Boolean, default: false },
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
